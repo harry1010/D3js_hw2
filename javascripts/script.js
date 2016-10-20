600f4ce3af990ffa64250b90f208d3de619e248d
OETPO\oetpo1 <javaoetpo@gmail.com>
2016-10-21 07:35:02 +0800

    initialize

---
+++ javascripts/script.js
@@ -0,0 +1,11 @@
+function showResult(msg1, msg2) {
+    var msg = msg1 + msg2;
+    return msg;
+    
+}
+
+function random(n, m)
+{
+    //console.log(n, m)
+    return Math.ceil(Math.random()*(m-n)+n)
+}
